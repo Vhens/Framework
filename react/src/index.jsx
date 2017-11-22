9a21fa8b9bbb53570157d35213897120201ad97f
wwSmile <2021054875@qq.com>
2017-11-22 18:07:35 +0800

    add base style

--- react/src/index.jsx
+++ react/src/index.jsx
@@ -1,7 +1,7 @@
 import React, {Component, PropTypes} from 'react';
 import ReactDOM, {render} from 'react-dom';
 const MOUNT_NODE = document.getElementById('app');
-
+import '../static/css/base.scss'
 import RouteMap from './routes/routes'
 
 render (
